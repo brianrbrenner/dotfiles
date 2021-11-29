835f73ac019e73a447afff42df7e99eaa65f65a3
brian <brian@machine.home>
2021-11-28 21:26:53 -0500

    install and services scripts

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# THIS INSTALL SCRIPT IS VERY BASIC AND FOR XFCE4 VOID LINUX INSTALLS ONLY
+
+# Applications and dependencies to allow either xfce4 with i3 or just i3 alone
+sudo xbps-install -Syu xtools tmux dmenu nitrogen i3-gaps neovim subversion NetworkManager network-manager-applet chromium alacritty zsh htop xfce4-pulseaudio-plugin xfce4-alsa-plugin bluez blueman unzip wget picom nerd-fonts-ttf font-awesome5 ripgrep cmake clang valgrind xrandr arandr python3 pasystray
+
+# change to zsh
+chsh -s $(which zsh)
+
+# install vim plug
+mkdir ~/.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# configuration
+cp -r ~/dotfiles/.config
+cp ~/dotfiles/.zshrc ~/
+cp ~/dotfiles/.tmux.conf ~/
+mkdir ~/Pictures
+cp -r ~/dotfiles/Wallpapers ~/Pictures/Wallpapers
