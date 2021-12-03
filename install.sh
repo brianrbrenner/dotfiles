c304ff644f6b7daa435600eaafe71d5385626da0
brianrbrenner <69250070+brianrbrenner@users.noreply.github.com>
2021-12-02 22:03:46 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # THIS INSTALL SCRIPT IS VERY BASIC AND FOR XFCE4 VOID LINUX INSTALLS ONLY
 
 # Applications and dependencies to allow either xfce4 with i3 or just i3 alone
-sudo xbps-install -Syu xtools tmux dmenu nitrogen i3-gaps neovim subversion NetworkManager network-manager-applet chromium alacritty zsh htop xfce4-pulseaudio-plugin xfce4-alsa-plugin bluez blueman unzip wget picom nerd-fonts-ttf font-awesome5 ripgrep cmake clang valgrind xrandr arandr python3 pasystray
+sudo xbps-install -Syu xtools tmux dmenu nitrogen i3-gaps neovim subversion NetworkManager network-manager-applet chromium alacritty zsh htop xfce4-pulseaudio-plugin xfce4-alsa-plugin bluez blueman unzip wget picom font-iosevka font-awesome5 ripgrep cmake clang valgrind xrandr python3 gcc rustup
 
 # change to zsh
 chsh -s $(which zsh)
