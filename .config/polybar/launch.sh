de6bc709f9804d19e2327ccf13b704739b0c4cea
Brian Brenner <bbrenne6@kent.edu>
2022-03-21 23:13:18 -0400

    bspwm now

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+#killall -q polybar
+# Launch main 
+
+# Wait until the processes have been shut down
+while pgrep polybar -x >/dev/null; do sleep 1; done
+
+# Launch polybar
+polybar main -c $(dirname $0)/config.ini &
+
+if [[ $(xrandr -q | grep 'HDMI1 connected') ]]; then
+	polybar external -c $(dirname $0)/config.ini &
+fi
