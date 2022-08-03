33352e67f41455086def985e3ef7c339c3fa3e6b
Brian Brenner <bbrenne6@kent.edu>
2022-08-02 23:10:58 -0400

    added required lint/formatter

--- .config/nvim/lua/ag/mason_lsp.lua
+++ .config/nvim/lua/ag/mason_lsp.lua
@@ -1,4 +1,4 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = { "sumneko_lua", "tsserver" }
+    ensure_installed = { "sumneko_lua", "tsserver", "eslint_d", "prettierd" }
 })
