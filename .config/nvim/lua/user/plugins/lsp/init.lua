d57fa6ed1834f359a827cb46769b635c4b4d235d
Brian Brenner <bbrenne6@kent.edu>
2022-12-01 23:28:40 -0500

    tidy up

--- .config/nvim/lua/user/plugins/lsp/init.lua
+++ .config/nvim/lua/user/plugins/lsp/init.lua
@@ -1,4 +1,4 @@
 require "user.plugins.lsp.mason"
 require("user.plugins.lsp.handlers").setup()
 require "user.plugins.lsp.null-ls"
-require "user.plugins.lsp.lspsaga"
+-- require "user.plugins.lsp.lspsaga"
