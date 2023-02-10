2964b5b401d0f029038ef927adfd4c3f8d225cea
Brian Brenner <bbrenne6@kent.edu>
2023-02-09 19:46:07 -0500

    fixed bars rendering

--- .config/scripts/dwm-bar/bar-functions/dwm_xbps.sh
+++ .config/scripts/dwm-bar/bar-functions/dwm_xbps.sh
@@ -3,11 +3,12 @@
 dwm_xbps() {
   updates=$(xbps-install -Mun 2> /dev/null | wc -l)
 
-  printf "%s" "$SEP1"
   if [ -n "$updates" ] && [ "$updates" -gt 0 ]; then
+      printf "%s" "$SEP1"
       echo "  $updates"
+      printf "%s\n" "$SEP2"
   else
       echo ""
   fi
-  printf "%s\n" "$SEP2"
 }
+
