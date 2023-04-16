016b78f8c23958f2a6b34312e4bfc4339103781d
Brian Brenner <bbrenne6@kent.edu>
2023-04-16 16:48:30 -0400

    added copilot

--- .config/lazyvim/lua/plugins/plugins.lua
+++ .config/lazyvim/lua/plugins/plugins.lua
@@ -45,4 +45,12 @@ return {
       colorscheme = "catppuccin",
     },
   },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup()
+    end,
+  },
 }
