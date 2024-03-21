3971375344fd6492d1a687f71e3cc2c79bcb9bf9
Brian Brenner <bbrenne6@kent.edu>
2024-03-20 21:35:40 -0400

    added wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,111 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.font = wezterm.font 'JetBrains Mono'
+config.color_scheme = 'Gruvbox dark, hard (base16)'
+
+config.default_workspace = "home"
+-- keymaps
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+  { key = "a", mods = "LEADER", action = act.SendKey { key = "a", mods = "CTRL" }},
+  { key = "c", mods = "LEADER", action = act.ActivateCopyMode },
+  -- Pane keybindings
+  { key = "-", mods = "LEADER",       action = act.SplitVertical { domain = "CurrentPaneDomain" } },
+  -- SHIFT is for when caps lock is on
+  { key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
+  { key = "h", mods = "LEADER",       action = act.ActivatePaneDirection("Left") },
+  { key = "j", mods = "LEADER",       action = act.ActivatePaneDirection("Down") },
+  { key = "k", mods = "LEADER",       action = act.ActivatePaneDirection("Up") },
+  { key = "l", mods = "LEADER",       action = act.ActivatePaneDirection("Right") },
+  { key = "x", mods = "LEADER",       action = act.CloseCurrentPane { confirm = true } },
+  { key = "z", mods = "LEADER",       action = act.TogglePaneZoomState },
+  { key = "s", mods = "LEADER",       action = act.RotatePanes "Clockwise" },
+  -- We can make separate keybindings for resizing panes
+  -- But Wezterm offers custom "mode" in the name of "KeyTable"
+  { key = "r", mods = "LEADER",       action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
+
+  -- Tab keybindings
+  { key = "n", mods = "LEADER",       action = act.SpawnTab("CurrentPaneDomain") },
+  { key = "LeftArrow", mods = "SHIFT",       action = act.ActivateTabRelative(-1) },
+  { key = "RightArrow", mods = "SHIFT",       action = act.ActivateTabRelative(1) },
+  { key = "t", mods = "LEADER",       action = act.ShowTabNavigator },
+  -- Key table for moving tabs around
+  { key = "m", mods = "LEADER",       action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
+
+  -- Lastly, workspace
+  { key = "w", mods = "LEADER",       action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
+}
+
+-- I can use the tab navigator (LDR t), but I also want to quickly navigate tabs with index
+for i = 1, 9 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "LEADER",
+    action = act.ActivateTab(i - 1)
+  })
+end
+
+config.key_tables = {
+  resize_pane = {
+    { key = "h",      action = act.AdjustPaneSize { "Left", 1 } },
+    { key = "j",      action = act.AdjustPaneSize { "Down", 1 } },
+    { key = "k",      action = act.AdjustPaneSize { "Up", 1 } },
+    { key = "l",      action = act.AdjustPaneSize { "Right", 1 } },
+    { key = "Escape", action = "PopKeyTable" },
+    { key = "Enter",  action = "PopKeyTable" },
+  },
+  move_tab = {
+    { key = "h",      action = act.MoveTabRelative(-1) },
+    { key = "j",      action = act.MoveTabRelative(-1) },
+    { key = "k",      action = act.MoveTabRelative(1) },
+    { key = "l",      action = act.MoveTabRelative(1) },
+    { key = "Escape", action = "PopKeyTable" },
+    { key = "Enter",  action = "PopKeyTable" },
+  }
+}
+
+-- Tab bar
+-- I don't like the look of "fancy" tab bar
+config.use_fancy_tab_bar = false
+config.status_update_interval = 1000
+wezterm.on("update-right-status", function(window, pane)
+  -- Workspace name
+  local stat = window:active_workspace()
+  -- It's a little silly to have workspace name all the time
+  -- Utilize this to display LDR or current key table name
+  if window:active_key_table() then stat = window:active_key_table() end
+  if window:leader_is_active() then stat = "LDR" end
+
+  -- Current working directory
+  local basename = function(s)
+    -- Nothign a little regex can't fix
+    return string.gsub(s, "(.*[/\\])(.*)", "%2")
+  end
+  local cwd = basename(pane:get_current_working_dir())
+  -- Current command
+  local cmd = basename(pane:get_foreground_process_name())
+
+  -- Let's add color to one of the components
+  window:set_right_status(wezterm.format({
+    -- Wezterm has a built-in nerd fonts
+    { Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
+    { Text = " | " },
+    { Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
+    { Text = " | " },
+    { Foreground = { Color = "FFB86C" } },
+    { Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
+    "ResetAttributes",
+    { Text = " | " },
+  }))
+end)
+
+-- and finally, return the configuration to wezterm
+return config
